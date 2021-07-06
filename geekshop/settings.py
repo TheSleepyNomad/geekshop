ebf3ea4ac3d2e1fbc276e08ff2059d496a16e2b3
TheSleepyNomad <iluxaan@mail.ru>
2021-07-06 11:26:57 +0300

    change settings.py and moved the folder templates folder

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'productsapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
