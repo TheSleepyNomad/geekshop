f71380d65595acc550f77305f77486ce71904152
TheSleepyNomad <iluxaan@mail.ru>
2021-07-06 11:22:12 +0300

    create products app

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_#dal-3v05340h@l!&5(&ea+ce)@d!%&g20ez6h5l%li+qd(z%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
