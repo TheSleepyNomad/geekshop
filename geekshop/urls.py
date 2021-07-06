2e7cf5409751c0a484652b19c8592fcd47a6ccfe
TheSleepyNomad <iluxaan@mail.ru>
2021-07-06 17:27:47 +0300

    homework_3 done

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin_log'),
     path('', index, name='index'),
     path('products/', include('productsapp.urls', namespace='products')),
 
