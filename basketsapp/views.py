dee489784a1a415db670979ae6d599ec3f437f6f
TheSleepyNomad <iluxaan@mail.ru>
2021-07-16 14:55:13 +0300

    refactoring views.py in users and baskets

--- basketsapp/views.py
+++ basketsapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from productsapp.models import Product
 from basketsapp.models import Basket
 
 
 # Create your views here.
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -16,3 +18,10 @@ def basket_add(request, product_id):
         basket.quantity += 1
         basket.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_del(request, id):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
