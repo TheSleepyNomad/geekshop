d43b3cf0a84dc9290239c21322b43293eba816c2
TheSleepyNomad <iluxaan@mail.ru>
2021-07-19 11:41:05 +0300

    add 'READ' for admin-users

--- admins/views.py
+++ admins/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
+from users.models import User
 # Create your views here.
 def index(request):
     return render(request, 'admins/index.html')
 
 def admin_users(request):
-    return render(request, 'admins/admin-users-read.html')
+    context = {
+        'users': User.objects.all(),
+    }
+    return render(request, 'admins/admin-users-read.html', context)
 
 def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html')
