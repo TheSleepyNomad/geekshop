8b368b68fc016bbc78e5f0350ecceac2645be0a9
TheSleepyNomad <iluxaan@mail.ru>
2021-07-19 12:39:54 +0300

    add 'UPDATE' and 'DELETE' for users

--- admins/forms.py
+++ admins/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserProfileForm
 from users.models import User
 
 
@@ -9,3 +9,7 @@ class UserAdminRegistrationForm(UserRegistrationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'image')
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': False, }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': False, }))
--- admins/views.py
+++ admins/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+
+
 # Create your views here.
 def index(request):
     return render(request, 'admins/index.html')
+
 
 def admin_users(request):
     context = {
@@ -12,9 +15,10 @@ def admin_users(request):
     }
     return render(request, 'admins/admin-users-read.html', context)
 
+
 def admin_users_create(request):
     if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST,files=request.FILES)
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('admins:admin_users'))
@@ -26,8 +30,22 @@ def admin_users_create(request):
                }
     return render(request, 'admins/admin-users-create.html', context)
 
+
 def admin_users_update(request, pk):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {'title': 'Редактирование пользователя', 'form': form, 'selected_user': selected_user}
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
 
 def admin_users_remove(request, pk):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_user = User.objects.get(id=pk)
+    selected_user.is_active = False
+    selected_user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
