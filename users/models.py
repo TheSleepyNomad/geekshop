118ee3a9653ff7ab497a27662f3bbffe229af790
TheSleepyNomad <iluxaan@mail.ru>
2021-09-29 09:50:09 +0300

    add language field in user model

--- users/models.py
+++ users/models.py
@@ -8,3 +8,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     image = models.ImageField(upload_to="users_images", blank=True)
     age = models.PositiveIntegerField(default=18, verbose_name="Возраст")
+    language = models.CharField(max_length=3,verbose_name='язык пользователя',default='ENG')
