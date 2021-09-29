8a1efad88b2e16be6b1c95aedf0e849e286ec2ff
TheSleepyNomad <iluxaan@mail.ru>
2021-09-29 09:16:10 +0300

    Add new field in user.model

--- users/models.py
+++ users/models.py
@@ -4,5 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
-    image = models.ImageField(upload_to='users_images', blank=True)
+    image = models.ImageField(upload_to="users_images", blank=True)
+    age = models.PositiveIntegerField(default=18, verbose_name="Возраст")
