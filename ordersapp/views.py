39c30946e34d2607a4ed20b45babaed40b0727c7
TheSleepyNomad <iluxaan@mail.ru>
2021-10-07 16:52:54 +0300

    create order_list.html | start dev OrderListView

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,11 +1,19 @@
+from django.db.models import fields
 from django.shortcuts import render
+from django.urls.base import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
+from .models import Order
 
 # Create your views here.
 
 
 class OrderList(ListView):
-    pass
+    model = Order
+    fields = []
+    success_url = reverse_lazy('order:list')
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user, is_active=True)
 
 
 class OrderCreate(CreateView):
