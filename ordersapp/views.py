210eebe33d82543b2d7528bfd4da05257663c03f
TheSleepyNomad <iluxaan@mail.ru>
2021-10-10 11:19:18 +0300

    add product in db / fix some bugs / write OrderCreate controller

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,8 +1,10 @@
+from ordersapp.forms import OrderItemsForm
 from django.db.models import fields
+from django.forms.models import inlineformset_factory
 from django.shortcuts import render
 from django.urls.base import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
-from .models import Order
+from .models import Order, OrderItem
 
 
 # Create your views here.
@@ -16,7 +18,22 @@ class OrderList(ListView):
 
 
 class OrderCreate(CreateView):
-    pass
+    model = Order
+    fields = []
+    success_url = reverse_lazy('orders:list')
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderCreate, self).get_context_data(**kwargs)
+        context['title'] = 'Geekshop : Создание заказа'
+        OrderFormSet = inlineformset_factory(
+            Order, OrderItem, form=OrderItemsForm, extra=1)
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST)
+        else:
+            formset = OrderFormSet()
+
+        context['orderitems'] = formset
+        return context
 
 
 class OrderUpdate(UpdateView):
