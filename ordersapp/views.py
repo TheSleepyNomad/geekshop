64b8a68548e5bd8f58e39a79e9412d1017de9f35
TheSleepyNomad <iluxaan@mail.ru>
2021-10-10 10:34:52 +0300

    migrate order and orderItem models / fix pattern templ bug

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -4,13 +4,12 @@ from django.urls.base import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 from .models import Order
 
+
 # Create your views here.
 
 
 class OrderList(ListView):
     model = Order
-    fields = []
-    success_url = reverse_lazy('order:list')
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user, is_active=True)
