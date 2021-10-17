758e566568c9265c6f76c14da48643e3bc1c328b
TheSleepyNomad <iluxaan@mail.ru>
2021-10-17 17:36:43 +0300

    refactor: return debug mode in true

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = 'django-insecure-_#dal-3v05340h@l!&5(&ea+ce)@d!%&g20ez6h5l%li+qd(z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
